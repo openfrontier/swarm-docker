873205d365645034787edfb7b518d20fb8ad19d5
thinkernel <thinkernel@gmail.com>
2016-02-15 14:07:02 +0800

    HOST_IP -> DISCOVERY_IP

--- startManager.sh
+++ startManager.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-HOST_IP=$1
+DISCOVERY_IP=$1
 docker run --name swarm-master \
     --restart=unless-stopped \
     -p 9999:2375 \
     -d swarm \
     manage \
-    etcd://${HOST_IP}:2379
+    etcd://${DISCOVERY_IP}:2379
