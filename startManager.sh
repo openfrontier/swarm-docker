f0be7cf4a8b9b6864380275060530978659ab5b9
thinkernel <thinkernel@gmail.com>
2016-06-29 09:19:05 +0000

    create HA swarm manager

--- startManager.sh
+++ startManager.sh
@@ -2,9 +2,12 @@
 set -e
 
 DISCOVERY_IP=$1
+PORT=${2:-9999}
 docker run --name swarm-master \
     --restart=unless-stopped \
-    -p 9999:2375 \
+    -p ${PORT}:2375 \
     -d swarm \
     manage \
+    --replication \
+    --advertise ${DISCOVERY_IP}:${PORT} \
     etcd://${DISCOVERY_IP}:2379
